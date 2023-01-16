3fa160d8b28905620a8384809dcf5c233901dbe9
whdcks420 <whdcks420@gmail.com>
2023-01-17 01:40:46 +0900

    test: 유저  업데이트 테스트

--- src/test/java/com/letseat/domain/user/UserAccountTest.java
+++ src/test/java/com/letseat/domain/user/UserAccountTest.java
@@ -31,53 +31,54 @@ class UserAccountTest {
     @BeforeEach
     public void createAccount() {
         UserAccount userAccount = UserAccount.builder()
-                .nickname("bellCold")
+                .nickname("Success")
                 .password(passwordEncoder.encode("12345678"))
                 .userGrade(BASIC)
                 .address("seoul")
                 .phone("010-1234-5678")
-                .email("whdcks420@gmail.com")
+                .email("Success@gmail.com")
                 .build();
 
         userRepository.save(userAccount);
     }
 
     @AfterEach
-    public void deleteAccount() {
+    public void rollBack() {
         userRepository.deleteAll();
     }
 
     @DisplayName("유저 생성 테스트")
     @Test
     public void userCreateTest() {
-        UserAccount findUser = userRepository.findByNickname("bellCold").orElseThrow(() -> new LetsEatException(DUPLICATE_RESOURCE));
-        assertThat("bellCold").isEqualTo(findUser.getNickname());
+        UserAccount findUser = userRepository.findByNickname("Success").orElseThrow(() -> new LetsEatException(DUPLICATE_RESOURCE));
+        assertThat("Success").isEqualTo(findUser.getNickname());
         assertThat("whdcks420").isNotEqualTo(findUser.getNickname());
         assertThat(passwordEncoder.matches("12345678", findUser.getPassword())).isTrue();
-    }
-
-    @DisplayName("이메일 또는 닉네임으로 유저 찾기 성공")
-    @Test
-    public void findUserByNickOrEmailSuccess() {
-        assertThat(userRepository.existsByNicknameOrEmail("bellCold", null)).isTrue();
-        assertThat(userRepository.existsByNicknameOrEmail(null, "whdcks420@gmail.com")).isTrue();
-    }
-
-    @DisplayName("이메일 또는 닉네임으로 유저 찾기 실패")
-    @Test
-    public void findUserByNickOrEmailFail() {
-        assertThat(userRepository.existsByNicknameOrEmail("bellColdFail", null)).isFalse();
-        assertThat(userRepository.existsByNicknameOrEmail(null, "whdcks420@gmail.comFail")).isFalse();
     }
 
     @DisplayName("회원 정보 수정 테스트")
     @Test
     public void updateUserAccount() {
-        UserAccount userAccount = userRepository.findById(1L).orElseThrow(() -> new LetsEatException(USER_NOT_FOUND));
-        userService.update(1L, new UserUpdateRequestDto("11112222", "경기도 시흥시띵띵띵", "010-5678-1234"));
+        UserAccount userAccount = userRepository.findByNickname("Success").orElseThrow(() -> new LetsEatException(USER_NOT_FOUND));
+        userService.update(userAccount.getId(), new UserUpdateRequestDto("11112222", "경기도", "010-5678-1234"));
 
-        assertThat(userAccount.getAddress()).isEqualTo("경기도 시흥시띵띵띵");
+        assertThat(userAccount.getAddress()).isEqualTo("경기도");
         assertThat(userAccount.getPhone()).isEqualTo("010-5678-1234");
         assertThat(passwordEncoder.matches("11112222", userAccount.getPassword())).isTrue();
+
+    }
+
+    @DisplayName("이메일 또는 닉네임 확인 테스트 성공")
+    @Test
+    public void findUserByNickOrEmailSuccess() {
+        assertThat(userRepository.existsByNicknameOrEmail("Success", null)).isTrue();
+        assertThat(userRepository.existsByNicknameOrEmail(null, "Success@gmail.com")).isTrue();
+    }
+
+    @DisplayName("이메일 또는 닉네임 확인 테스트 실패")
+    @Test
+    public void findUserByNickOrEmailFail() {
+        assertThat(userRepository.existsByNicknameOrEmail("Fail", null)).isFalse();
+        assertThat(userRepository.existsByNicknameOrEmail(null, "Fail@gmail.com")).isFalse();
     }
 }
