304b20b33aa246ab3e521370ba56462c4584c94a
whdcks420 <whdcks420@gmail.com>
2023-01-16 18:21:58 +0900

    feat: DB 기초 설정

--- src/main/java/com/letseat/LetsEatApplication.java
+++ src/main/java/com/letseat/LetsEatApplication.java
@@ -1,13 +1,13 @@
-package com.example.letseat;
+package com.letseat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LetseatApplication {
+public class LetsEatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LetseatApplication.class, args);
+        SpringApplication.run(LetsEatApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/letseat/global/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.letseat.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/").permitAll();
+
+        return http.build();
+    }
+
+}
